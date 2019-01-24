2cb7d2f776cedd91dd5d45ea70fd556a62508e84
Imran <imran1995bd@gmail.com>
2019-01-24 18:16:44 +0600

    Initial commit

---
+++ src/main/java/com/example/pos/api/posAPI/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.example.pos.api.posAPI.dao;
+
+import com.example.pos.api.posAPI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+
+}
---
+++ src/main/java/com/example/pos/api/posAPI/model/User.java
@@ -0,0 +1,57 @@
+package com.example.pos.api.posAPI.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+
+    public int id;
+
+    public String username;
+
+    public String password;
+
+    public User() {
+    }
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
